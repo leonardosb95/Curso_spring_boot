c9e9d438e3240412ed153f6ec4a868b657567bd6
Leonardo Bispo <lbispo@a5-solutions.local>
2019-10-11 07:16:29 -0300

    first commit

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,35 @@
+package br.com.alura.forum.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+
+@RestController
+public class TopicosController {
+
+	@Autowired
+	private TopicoRepository topicoRepository;
+
+	@RequestMapping("/topicos")
+	public java.util.List<TopicoDto> lista(String nomeCurso) {
+		System.out.println(nomeCurso);
+		if (nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+
+		}
+		else {
+			
+			List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);//Busca pela entidade curso e pelo atributo nome
+			return TopicoDto.converter(topicos);
+		}
+
+	}
+
+}
---
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -0,0 +1,23 @@
+package br.com.alura.forum.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.alura.forum.model.Topico;
+
+@Repository
+public interface TopicoRepository extends JpaRepository<Topico,Long> {
+
+
+	List<Topico> findByCurso_Nome(String nomeCurso);
+
+	List<Topico> findByTitulo(String nomeCurso);
+	
+	
+	
+	
+	
+
+}
